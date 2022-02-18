310514be883deb25c0a75894c03213e0c16b9034
Renan <renan.vamo@gmail.com>
2022-02-18 15:59:39 -0300

    refactor: envia a placa para ser pesquisada

--- src/controller/locationController.js
+++ src/controller/locationController.js
@@ -2,7 +2,7 @@ const locationService = require('../services/locationService.js');
 
 const getLogByDate = async (req, res) => {
   const { placa } = req.query;
-  const result = await locationService.getLogByDate();
+  const result = await locationService.getLogByDate(placa);
 
   res.status(200).json(result);
 };
