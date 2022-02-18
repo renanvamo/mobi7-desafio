fd39d5e9e1ddc170e889d5319496cfa9433f49ff
Renan <renan.vamo@gmail.com>
2022-02-18 16:38:55 -0300

    feat: adiciona logica ao service para verificar se o veiculo com a placa solicitada esteve proximo de um ponto de interesse

--- src/services/locationService.js
+++ src/services/locationService.js
@@ -6,10 +6,21 @@ const getLogByDate = async (placa) => {
   const positions = await locationModel.getPositionsByPlate(placa);
   const pois = await locationModel.getPois();
 
-  //necessário implementar a lógica para verificar se as coordenadas de posicao, estao dentro do radio de pois
-  const result = 'O veículo TESTE001 estava no dia 19/12/18 às 15h07m19s a 10 km/h na posição (51.549662,-25.5244493) com ignição ligada.';
 
-  return { result };
+  //necessário implementar a lógica para verificar se as coordenadas de posicao, estao dentro do raio de pois
+  // const result = 'O veículo TESTE001 estava no dia 19/12/18 às 15h07m19s a 10 km/h na posição (51.549662,-25.5244493) com ignição ligada.';
+  const result = [];
+  for (let i = 0; i < pois.length; i += 1) {
+    for (let j = 0; j < positions.length; j += 1) {
+      if (getDistanceFromCoord(positions[j].latitude, positions[j].longitude, pois[i].latitude, pois[i].longitude, pois[i].raio)) {
+        const log = `O veículo ${placa} estava no dia ${positions[j].data_posicao} à ${positions[j].velocidade} km/h na posição (${positions[j].latitude} ${positions[j].longitude}) com ignição ${positions[j].ignicao}`
+        
+        await result.push(log)
+      }
+    }
+  }
+
+  return { [placa]: result };
 }
 
 module.exports = {
