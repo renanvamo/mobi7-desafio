916df9f1f4903b04a41403e23968de96b19057c3
Renan <renan.vamo@gmail.com>
2022-02-18 20:21:35 -0300

    refactor: altera um loop para a hof filter, na funcao filterPositionsByDate

--- src/utils/filterPositionsByDate.js
+++ src/utils/filterPositionsByDate.js
@@ -2,13 +2,14 @@ const formatDate = require("./formatDate");
 
 const filterPositionsByDate = (positions, date) => {
   const positionsFiltered = [];
-  for (let i = 0; i < positions.length; i += 1) {
-    const formattedDate = formatDate(new Date(positions[i].data_posicao)).split('/').join('-');
 
+  positions.filter(position => {
+    const formattedDate = formatDate(new Date(position.data_posicao)).split('/').join('-');
+  
     if (date.match(formattedDate)) {
-      positionsFiltered.push(positions[i]);
+      positionsFiltered.push(position);
     };
-  };
+  });
   return positionsFiltered;
 };
 
