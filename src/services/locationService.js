fb9a87567ced546b2fa55c8f3b4c40f728ad5fc9
Renan <renan.vamo@gmail.com>
2022-02-18 20:09:53 -0300

    feat: adiciona logica para funcao que busca todas as posicoes pela placa, e depois faz um filtro pela data

--- src/services/locationService.js
+++ src/services/locationService.js
@@ -1,5 +1,6 @@
 const locationModel = require('../models/locationModel');
 const checkPositionsAndPois = require('../utils/checkPositionsAndPois');
+const formatDate = require('../utils/formatDate');
 
 const getLogsByPlate = async (placa) => {
   const positions = await locationModel.getPositionsByPlate(placa);
@@ -11,10 +12,19 @@ const getLogsByPlate = async (placa) => {
 };
 
 const getLogsByPlateAndDate = async (placa, date) => {
-  const positions = await locationModel.getPositionsByPlateAndDate(placa, date);
+  const positions = await locationModel.getPositionsByPlate(placa);
   const pois = await locationModel.getPois();
 
-  const log = checkPositionsAndPois(positions, pois, placa);
+  const positionsFiltered = [];
+  for (let i = 0; i < positions.length; i += 1) {
+    const formattedDate = formatDate(new Date(positions[i].data_posicao)).split('/').join('-');
+
+    if (date.match(formattedDate)) {
+      positionsFiltered.push(positions[i]);
+    };
+  };
+
+  const log = checkPositionsAndPois(positionsFiltered, pois, placa);
 
   return { [placa]: log };
 };
