680c4fe0537ae5c8186989388767d4440dc220b8
Renan <renan.vamo@gmail.com>
2022-02-18 15:03:57 -0300

    fix: adiciona await nas linhas que inserem dados no nas collections

--- src/database/createCollections.js
+++ src/database/createCollections.js
@@ -1,21 +1,20 @@
 const connection = require('./connection');
 const csvReader = require('./csvReader');
-// const positionsCSV = require('../database/filesCSV/posicoes.csv');
-// const poisCSV = require('../database/filesCSV/base_pois_def.csv');
 
 const createCollections = async () => {
   const positions = await csvReader("posicoes.csv");
   const pois = await csvReader("base_pois_def.csv");
 
-  await drop();
+  await dropDbMobi7();
 
-  connection().then((db) => db.collection('positions').insertMany(positions));
-  connection().then((db) => db.collection('pois').insertMany(pois));
+  await connection().then((db) => db.collection('positions').insertMany(positions));
+  await connection().then((db) => db.collection('pois').insertMany(pois));
+  console.log('Os bancos de dados \'positions\' e \'pois\' foram preenchidos');
+  process.exit();
 };
 
-const drop = async () => {
+const dropDbMobi7 = async () => {
   await connection().then((db) => db.dropDatabase());
 };
 
 createCollections();
-process.exit();
