8615666ae07dd375f13ed7fd68aa64f4f7c85ef0
Renan <renan.vamo@gmail.com>
2022-02-18 16:03:57 -0300

    refactor: ajusta as funcoes de models para fazer um find dos dados do banco

--- src/models/locationModel.js
+++ src/models/locationModel.js
@@ -2,17 +2,23 @@ const connection = require('../database/connection');
 
 const getDocuments = (collection) => connection().then((db) => db.collection(collection));
 
-const getPositions = async () => {
-  const positions = await getDocuments('positions');
+const getPositionsByPlate = async (placa) => {
+  const collection = await getDocuments('positions');
+  const positions = await collection.find({ placa }).toArray();
+
+  console.log(positions.length);
   return positions;
 };
 
 const getPois = async () => {
-  const pois = await getDocuments('pois');
+  const collection = await getDocuments('pois');
+  const pois = await collection.find().toArray();
+
+  console.log(pois.length);
   return pois;
 }
 
 module.exports = {
-  getPositions,
+  getPositionsByPlate,
   getPois
 };
