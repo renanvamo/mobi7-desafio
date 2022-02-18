fb04290c8d30bc1ccc65e2b75fd1c533516bd002
Renan <renan.vamo@gmail.com>
2022-02-18 19:20:28 -0300

    refactor: adiciona um novo controler, e muda nome do controler existente

--- src/controller/locationController.js
+++ src/controller/locationController.js
@@ -1,12 +1,22 @@
 const locationService = require('../services/locationService.js');
 
-const getLogByDate = async (req, res) => {
+const getLogsByPlate = async (req, res) => {
   const { placa } = req.query;
-  const result = await locationService.getLogByDate(placa);
+  const result = await locationService.getLogsByPlate(placa);
+
+  res.status(200).json(result);
+};
+
+const getLogsByPlateAndDate = async (req, res) => {
+  const { placa } = req.query;
+  const { date } = req.params;
+   
+  const result = await locationService.getLogsByPlateAndDate(placa, date);
 
   res.status(200).json(result);
 };
 
 module.exports = {
-  getLogByDate
+  getLogsByPlateAndDate,
+  getLogsByPlate,
 };
