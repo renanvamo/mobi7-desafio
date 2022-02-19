b68657c27ebc73a6516ae23d6f50ea262a37f55e
Renan <renan.vamo@gmail.com>
2022-02-18 23:27:12 -0300

    refactor: remove a validacao do controller, pois foi criado um middlewares para eles

--- src/controller/locationController.js
+++ src/controller/locationController.js
@@ -1,16 +1,8 @@
 const locationService = require('../services/locationService.js');
-const plateValidation = require('../validations/plateValidation.js');
-const datePlateValidation = require('../validations/datePlateValidation.js');
 
 const getLogsByPlate = async (req, res) => {
   const { placa } = req.query;
   
-  const { error: validationError } = plateValidation
-  ({ placa });
-  if (validationError) {
-    return res.status(400).json({ erro: validationError.details[0].message });
-  };
-
   const result = await locationService.getLogsByPlate(placa);
   
   res.status(200).json(result);
@@ -20,11 +12,6 @@ const getLogsByPlateAndDate = async (req, res) => {
   const { placa } = req.query;
   const { date } = req.params;
 
-  const { error: validationError } = datePlateValidation({ placa, date });
-  if (validationError) {
-    return res.status(400).json({ erro: validationError.details[0].message });
-  };
-   
   const result = await locationService.getLogsByPlateAndDate(placa, date);
 
   res.status(200).json(result);
