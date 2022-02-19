6da6fd08f4b88005dbf82d9842ef6f45d7375ceb
Renan <renan.vamo@gmail.com>
2022-02-18 23:27:26 -0300

    feat: chama as validacoes nas rotas

--- src/routes/locationRoutes.js
+++ src/routes/locationRoutes.js
@@ -1,8 +1,9 @@
 const locationRoute = require('express').Router();
+const validations = require('../middlewares/validations')
 
 const { getLogsByPlateAndDate, getLogsByPlate } = require('../controller/locationController');
 
-locationRoute.get('/location', getLogsByPlate);
-locationRoute.get('/location/:date', getLogsByPlateAndDate);
+locationRoute.get('/location', validations.plate, getLogsByPlate);
+locationRoute.get('/location/:date', validations.dateAndPlate, getLogsByPlateAndDate);
 
 module.exports = locationRoute;
