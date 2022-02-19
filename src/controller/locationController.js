5816f54672395bfd3fbf0527b5fe4cb12d2b1580
Renan <renan.vamo@gmail.com>
2022-02-18 23:10:58 -0300

    feat: chama a validacao dentro de cada controller

--- src/controller/locationController.js
+++ src/controller/locationController.js
@@ -1,15 +1,29 @@
 const locationService = require('../services/locationService.js');
+const plateValidation = require('../validations/plateValidation.js');
+const datePlateValidation = require('../validations/datePlateValidation.js');
 
 const getLogsByPlate = async (req, res) => {
   const { placa } = req.query;
-  const result = await locationService.getLogsByPlate(placa);
+  
+  const { error: validationError } = plateValidation
+  ({ placa });
+  if (validationError) {
+    return res.status(400).json({ erro: validationError.details[0].message });
+  };
 
+  const result = await locationService.getLogsByPlate(placa);
+  
   res.status(200).json(result);
 };
 
 const getLogsByPlateAndDate = async (req, res) => {
   const { placa } = req.query;
   const { date } = req.params;
+
+  const { error: validationError } = datePlateValidation({ placa, date });
+  if (validationError) {
+    return res.status(400).json({ erro: validationError.details[0].message });
+  };
    
   const result = await locationService.getLogsByPlateAndDate(placa, date);
 
