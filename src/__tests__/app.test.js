8016e276522f4eaab0c1e2e51e1aec2930442538
Renan <renan.vamo@gmail.com>
2022-02-19 18:21:48 -0300

    test: adiciona um teste com uma placa invalida, e arruma titulo de um teste

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -6,6 +6,8 @@ const { expect } = chai;
 describe('testa a rota \'/location\'', () => {
   let connection;
   const placaCorreta = 'TESTE001';
+  const placaIncorreta1 = 'ABC';
+  const placaIncorreta2 = 'ABC123456789';
 
   before(() => {
     chai.use(chaiHttp);
@@ -27,8 +29,8 @@ describe('testa a rota \'/location\'', () => {
           done();
         });
     });
-  
-    it(`deve retornar \'724\' documentos'`, (done) => {
+
+    it(`deve retornar \'564\' documentos'`, (done) => {
       connection
         .query({ placa: placaCorreta})
         .end((_err, res) => {
@@ -42,6 +44,20 @@ describe('testa a rota \'/location\'', () => {
         .query({ placa: placaCorreta})
         .end((_err, res) => {
           expect(res.body[placaCorreta][0]).to.equal('O veículo TESTE001 estava no dia 19/12/2018 às 15:22:01 à 28 km/h na posição (-25.56742701740896 -51.47653363645077) com ignição ligada');
+          done();
+        });
+    });
+  });
+
+  describe(`pesquisando por uma placa incorreta \'${placaIncorreta1}\'`, () => {
+    it(`deve retornar status code \'400\', e ter uma chave \'erro\', `, (done) => {
+      connection
+        .query({ placa: placaIncorreta1})
+        .end((_err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property('erro');
+          expect(res.body.erro).to.be.a('string');
+          expect(res.body.erro).to.equal('"placa" deve ter no mínimo 7 caracteres');
           done();
         });
     });
