e3b3f32c3d76837b8b7cbd0ce7b29b011a10b8d3
Renan <renan.vamo@gmail.com>
2022-02-19 18:01:41 -0300

    tests: adiciona testes para verificar quantidade de documentos de retorno, e qual o primeiro elemento

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -16,14 +16,34 @@ describe('testa a rota \'/location\'', () => {
       .get('/location')
   });
 
-  it(`pesquisando pela placa \'${placaCorreta}\', deve retornar status code \'200\', e ter uma chave igual a placa - \'${placaCorreta}\'`, (done) => {
-    // teste deve passar
-    connection
-      .query({ placa: placaCorreta})
-      .end((_err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.have.property(placaCorreta);
-        done();
-      });
+  describe(`pesquisando pela placa \'${placaCorreta}\'`, () => {  
+    it(`deve retornar status code \'200\', e ter uma chave igual a placa - \'${placaCorreta}\', e seu valor um array`, (done) => {
+      connection
+        .query({ placa: placaCorreta})
+        .end((_err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.property(placaCorreta);
+          expect(res.body[placaCorreta]).to.be.an('array');
+          done();
+        });
+    });
+  
+    it(`deve retornar \'724\' documentos'`, (done) => {
+      connection
+        .query({ placa: placaCorreta})
+        .end((_err, res) => {
+          expect(res.body[placaCorreta]).to.have.length(564);
+          done();
+        });
+    });
+
+    it(`o primeiro elemento da chave ${placaCorreta}, deve ser \'O veículo TESTE001 estava no dia 19/12/2018 às 15:22:01 à 28 km/h na posição (-25.56742701740896 -51.47653363645077) com ignição ligada\'`, (done) => {
+      connection
+        .query({ placa: placaCorreta})
+        .end((_err, res) => {
+          expect(res.body[placaCorreta][0]).to.equal('O veículo TESTE001 estava no dia 19/12/2018 às 15:22:01 à 28 km/h na posição (-25.56742701740896 -51.47653363645077) com ignição ligada');
+          done();
+        });
+    });
   });
 });
